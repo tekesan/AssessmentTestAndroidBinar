7941ee87ba7728f64e1dc1080f35518078f3c2eb
Teke <fatih.avila@gmail.com>
2017-06-01 09:24:57 +0700

    test

--- app/src/main/java/com/mangkot/pencatatantoko/HalamanUtama.java
+++ app/src/main/java/com/mangkot/pencatatantoko/HalamanUtama.java
@@ -1,7 +1,11 @@
 package com.mangkot.pencatatantoko;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import static com.mangkot.pencatatantoko.R.styleable.View;
 
 public class HalamanUtama extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class HalamanUtama extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman_utama);
+
     }
+
+    public void reset(View V){
+        startActivity(new Intent(HalamanUtama.this, HalamanUtama.class));
+    }
+
+
 }
+
+
