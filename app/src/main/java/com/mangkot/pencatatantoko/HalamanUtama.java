b16a7752d89bee3a637c82182f95410acb17f6c3
Teke <fatih.avila@gmail.com>
2017-06-01 09:12:29 +0700

    test

---
+++ app/src/main/java/com/mangkot/pencatatantoko/HalamanUtama.java
@@ -0,0 +1,13 @@
+package com.mangkot.pencatatantoko;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HalamanUtama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halaman_utama);
+    }
+}
--- app/src/main/java/com/mangkot/pencatatantoko/MainActivity.java
+++ app/src/main/java/com/mangkot/pencatatantoko/MainActivity.java
@@ -1,7 +1,10 @@
 package com.mangkot.pencatatantoko;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
+    public void masuk(View V){
+        Intent intent = new Intent(getApplicationContext(), HalamanUtama.class);
+        startActivity(intent);
+    }
+
 }
+
+
