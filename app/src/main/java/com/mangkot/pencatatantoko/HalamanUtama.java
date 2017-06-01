68ce5d4e6b67c9670766f9e04b58d1eac505a761
Teke <fatih.avila@gmail.com>
2017-06-01 09:59:42 +0700

    final

--- app/src/main/java/com/mangkot/pencatatantoko/HalamanUtama.java
+++ app/src/main/java/com/mangkot/pencatatantoko/HalamanUtama.java
@@ -4,7 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 
+import static com.mangkot.pencatatantoko.R.id.editText;
+import static com.mangkot.pencatatantoko.R.id.editText7;
 import static com.mangkot.pencatatantoko.R.styleable.View;
 
 public class HalamanUtama extends AppCompatActivity {
@@ -16,10 +20,31 @@ public class HalamanUtama extends AppCompatActivity {
 
     }
 
-    public void reset(View V){
-        startActivity(new Intent(HalamanUtama.this, HalamanUtama.class));
+    public void reset(View V) {
+
+        Intent intent = getIntent();
+        overridePendingTransition(0, 0);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        finish();
+
+        overridePendingTransition(0, 0);
+        startActivity(intent);
     }
 
+    public void input(View v){
+        final EditText nameField = (EditText) findViewById(R.id.editText7);
+        String nama = nameField.getText().toString();
+
+        final EditText kuantitasField = (EditText) findViewById(R.id.editText13);
+        String kuantitas = kuantitasField.getText().toString();
+
+        final EditText pemasokField = (EditText) findViewById(R.id.editText8);
+        String pemasok = pemasokField.getText().toString();
+
+        final Spinner tglField = (Spinner) findViewById(R.id.datePicker);
+        String tgl = tglField.getSelectedItem().toString();
+        
+    }
 
 }
 
